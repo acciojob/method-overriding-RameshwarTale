1b6bb9cfe6d272775b0034686dd676754729984d
Rameshwar Tale <ramgtale21@gmail.com>
2024-02-10 11:01:50 +0530

    Changes make

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+class A{
+
+    public String meth(){
+        return "Invoking method from class A";
+    }
+
+
+}
+
+class B extends A{
+
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
@@ -8,26 +23,9 @@ public class Main {
         String str = obj.meth();
         System.out.println(str);
 
-        String str1 = obj.meth();
-        System.out.println(str1);
-
-    }
-
-    public static class A{
-
-        String str = meth();
-        public String meth(){
-            return "class A meth";
-        }
-
-
-    }
-
-   public static class B extends A{
-
-        public String meth(){
-            return "Method is overridden in Extendend class B";
-        }
     }
 }
+
+
+
 
