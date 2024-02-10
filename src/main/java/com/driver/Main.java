ac19c22d336f032b5a2deb6b616a4ad3edbc339a
Rameshwar Tale <ramgtale21@gmail.com>
2024-02-10 11:28:05 +0530

    add file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,13 @@
 package com.driver;
 
 class A{
-
-    public String meth(){
+     public String meth(){
         return "Invoking method from class A";
     }
-
-
 }
 
 class B extends A{
-
-    public String meth(){
+     public String meth(){
         return "Method is overridden in Extendend class B";
     }
 }
@@ -23,7 +19,8 @@ public class Main {
         String str = obj.meth();
         System.out.println(str);
 
-        String str1 = obj.meth();
+        B obj1 = new B();
+        String str1 = obj1.meth();
         System.out.println(str1);
 
     }
