97228dae823611029e0515f60a3e5c43c5a3b068
Rameshwar Tale <ramgtale21@gmail.com>
2024-02-10 10:46:26 +0530

    Changes Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        B obj = new B();
+
+        String str = obj.meth();
+        System.out.println(str);
+
+        String str1 = obj.meth();
+        System.out.println(str1);
+
+    }
+}
+
+class A{
+    public String meth(){
+        return "class A meth";
+    }
+
+    String str = meth();
+}
+
+class B extends A{
+
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
